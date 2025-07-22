4b866b89473e4e2adfb1f3aed129fde3f3196401
jdavidIP <ibapol2003@gmail.com>
2025-07-22 12:16:03 -0400

    Added delete and add functionality for transactions

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,9 @@ import TransactionList from "./components/TransactionList.vue";
 import AddTransaction from "./components/AddTransaction.vue";
 
 import { ref, computed } from "vue";
+import { useToast } from "vue-toastification";
+
+const toast = useToast();
 
 const transactions = ref([
   { id: 1, text: "Flower", amount: -18.99 },
@@ -27,7 +30,7 @@ const income = computed(() => {
       return acc + transaction.amount;
     }, 0)
     .toFixed(2);
-});
+}, 0);
 
 const expenses = computed(() => {
   return transactions.value
@@ -36,15 +39,42 @@ const expenses = computed(() => {
       return acc + transaction.amount;
     }, 0)
     .toFixed(2);
-});
+}, 0);
+
+const handleDeleteTransaction = (id) => {
+  transactions.value = transactions.value.filter(
+    (transaction) => transaction.id !== id
+  );
+
+  console.log(id);
+
+  toast.success("Transaction deleted");
+};
+
+const handleNewTransaction = (transactionData) => {
+  transactions.value.push({
+    id: generateID(),
+    text: transactionData.text,
+    amount: transactionData.amount,
+  });
+
+  toast.success("Transaction added");
+};
+
+const generateID = () => {
+  return Math.floor(Math.random() * 100000);
+};
 </script>
 
 <template>
   <Header />
   <div class="container">
-    <Balance :total="total" />
-    <IncomeExpenses :income="income" :expenses="expenses" />
-    <TransactionList :transactions="transactions" />
-    <AddTransaction />
+    <Balance :total="+total" />
+    <IncomeExpenses :income="+income" :expenses="+expenses" />
+    <TransactionList
+      @transactionDeleted="handleDeleteTransaction"
+      :transactions="transactions"
+    />
+    <AddTransaction @transactionSubmitted="handleNewTransaction" />
   </div>
 </template>
--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,12 +1,16 @@
 <script setup>
-import { defineProps } from "vue";
-
 const props = defineProps({
   transactions: {
     type: Array,
     required: true,
   },
 });
+
+const emit = defineEmits(["transactionDeleted"]);
+
+const handleDelete = (id) => {
+  emit("transactionDeleted", id);
+};
 </script>
 
 <template>
@@ -18,7 +22,9 @@ const props = defineProps({
       :class="transaction.amount < 0 ? 'minus' : 'plus'"
     >
       {{ transaction.text }} <span>{{ transaction.amount }}</span
-      ><button class="delete-btn">x</button>
+      ><button @click="handleDelete(transaction.id)" class="delete-btn">
+        x
+      </button>
     </li>
   </ul>
 </template>
