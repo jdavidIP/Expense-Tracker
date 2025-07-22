3dd370c5876ea25535667a95a79e4b26a63ddc76
jdavidIP <ibapol2003@gmail.com>
2025-07-22 11:50:12 -0400

    Added props for IncomeExpenses and TransactionList components

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,50 @@
-<script>
+<script setup>
 import Header from "./components/Header.vue";
 import Balance from "./components/Balance.vue";
 import IncomeExpenses from "./components/IncomeExpenses.vue";
 import TransactionList from "./components/TransactionList.vue";
 import AddTransaction from "./components/AddTransaction.vue";
 
-export default {
-  components: {
-    Header,
-    Balance,
-    IncomeExpenses,
-    TransactionList,
-    AddTransaction,
-  },
-};
+import { ref, computed } from "vue";
+
+const transactions = ref([
+  { id: 1, text: "Flower", amount: -18.99 },
+  { id: 2, text: "Salary", amount: 1998.99 },
+  { id: 3, text: "Video Game", amount: -105.29 },
+  { id: 4, text: "Gift", amount: 50.0 },
+]);
+
+const total = computed(() => {
+  return transactions.value.reduce((acc, transaction) => {
+    return acc + transaction.amount;
+  }, 0);
+});
+
+const income = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.amount > 0)
+    .reduce((acc, transaction) => {
+      return acc + transaction.amount;
+    }, 0)
+    .toFixed(2);
+});
+
+const expenses = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.amount < 0)
+    .reduce((acc, transaction) => {
+      return acc + transaction.amount;
+    }, 0)
+    .toFixed(2);
+});
 </script>
 
 <template>
   <Header />
   <div class="container">
-    <Balance />
-    <IncomeExpenses />
-    <TransactionList />
+    <Balance :total="total" />
+    <IncomeExpenses :income="income" :expenses="expenses" />
+    <TransactionList :transactions="transactions" />
     <AddTransaction />
   </div>
 </template>
--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,7 +1,24 @@
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  transactions: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
+
 <template>
   <h3>History</h3>
   <ul id="list" class="list">
-    <li class="minus"></li>
-    <li class="plus"></li>
+    <li
+      v-for="transaction in transactions"
+      :key="transaction.id"
+      :class="transaction.amount < 0 ? 'minus' : 'plus'"
+    >
+      {{ transaction.text }} <span>{{ transaction.amount }}</span
+      ><button class="delete-btn">x</button>
+    </li>
   </ul>
 </template>
